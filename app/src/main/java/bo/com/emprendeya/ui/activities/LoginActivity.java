332a2d71b818ee529fa391aa7328df3e229a00ca
andres-vasquuez-coderoad-com <andres.vasquez@mojix.com>
2020-10-23 08:53:21 -0400

    Firebase> User in the database.

--- app/src/main/java/bo/com/emprendeya/ui/activities/LoginActivity.java
+++ app/src/main/java/bo/com/emprendeya/ui/activities/LoginActivity.java
@@ -223,6 +223,7 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(context, StartupListActivity.class);
         intent.putExtra(Constants.KEY_UUID, user.getUuid());
         intent.putExtra(Constants.KEY_DISPLAY_NAME, user.getDisplayName());
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 }
